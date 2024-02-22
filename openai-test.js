6239318c43714899ac8c540b3ff80cb4eb9ee778
Utkarsh Kumar <2017csb1116@iitrpr.ac.in>
2024-02-22 08:46:32 +0530

    6th

--- openai-test.js
+++ openai-test.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser:true});
+// const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser:true});
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 export default async function query(input) {
   const completion = await openai.chat.completions.create({
